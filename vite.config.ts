7df5f06e70794b9c14a5b1cbed7c074f40cfec93
skyArony <1450872874@qq.com>
2023-08-28 17:44:43 +0800

    chore: 引入 Element Plus 和 Ant Design Vue

--- vite.config.ts
+++ vite.config.ts
@@ -3,10 +3,30 @@ import { fileURLToPath, URL } from "node:url";
 import UnoCSS from "unocss/vite";
 import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
+import AutoImport from "unplugin-auto-import/vite";
+import Components from "unplugin-vue-components/vite";
+import {
+  AntDesignVueResolver,
+  ElementPlusResolver,
+} from "unplugin-vue-components/resolvers";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue(), UnoCSS()],
+  plugins: [
+    vue(),
+    UnoCSS(),
+    AutoImport({
+      imports: ["vue", "vue-router", "@vueuse/core"],
+      resolvers: [ElementPlusResolver(), AntDesignVueResolver()],
+      eslintrc: {
+        enabled: true,
+      },
+    }),
+    Components({
+      resolvers: [ElementPlusResolver(), AntDesignVueResolver()],
+      dts: true,
+    }),
+  ],
   resolve: {
     alias: {
       "@": fileURLToPath(new URL("./src", import.meta.url)),
