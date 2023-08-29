48ea780926925219992eb62d33b0cddfb74ff095
skyArony <1450872874@qq.com>
2023-08-29 20:44:50 +0800

    wip: Chat UI 开发中

--- src/views/ChatView.vue
+++ src/views/ChatView.vue
@@ -1,6 +1,6 @@
 <template>
-  <div class="flex items-center min-h-100vh">
-    <h1>This is chat page</h1>
+  <div class="w-full h-100vh bg-cyan-300 flex justify-center items-center">
+    <div class="w-1200px h-1000px max-w-80% max-h-80% h-800px  shadow-md rounded-xl bg-red-300"></div>
   </div>
 </template>
 
